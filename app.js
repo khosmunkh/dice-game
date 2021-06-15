32b4e93b042ace29c34a1911e3ab87d8aa4e2abb
Khosmunkh <khosmunkhreal@gmail.com>
2021-06-15 11:59:02 +0800

    eeljiig n solidog bolgono

--- app.js
+++ app.js
@@ -18,7 +18,9 @@ var roundScore = 0;
 
 
 // Шооны аль талаараа буусныг хадгалах хувьсагч, 1-6 утгыг энэ хувьсагчид санамсаргүйгээр үүсгэж өгнө
-var dice =  
+
+
+
 //window.document.querySelector('#score-0').textContent = dice;
 //Програм эхлэхэд бэлтгье  
 document.getElementById('current-0').textContent = '0';
@@ -26,16 +28,34 @@ document.getElementById('current-1').textContent = '0';
 document.getElementById('score-0').textContent = '0';
 document.getElementById('score-1').textContent = '0';
 
-document.querySelector('.btn-roll').addEventListener("click",shooShid) 
 var diceDom = document.querySelector(".dice");
 diceDom.style.display = 'none';
-function shooShid()
-{
-    diceDom.style.display = "block";
+// Шоог шидэх эвент листенэр 
+
+document.querySelector('.btn-roll').addEventListener("click" ,function() 
+{   
     var diceNumber = Math.floor(Math.random()*6) + 1;
-    diceDom.src ='dice-' + diceNumber +'.png'
+    
+    diceDom.style.display = "block";
+    
+    diceDom.src ='dice-' + diceNumber +'.png';
+
+    if(diceNumber !== 1){
+        //1- ээс ялгаатай тоо буулааа
+        roundScore = roundScore + diceNumber; 
+        document.getElementById('current-' + activePlayer).textContent = roundScore;
+    }
+    else{
+        roundScore = 0;
+        document.getElementById('current-' + activePlayer).textContent = 0;
+        activePlayer ===0 ? (activePlayer = 1) : (activePlayer = 0);
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+        diceDom.style.display = "none";
+
+    }
+});
 
 
-}
-console.log('Шоо :', dice);
+
 
