39bc196c967e2d924bdf56bdd2b1b3028b7a61ab
Khosmunkh <khosmunkhreal@gmail.com>
2021-06-15 15:06:50 +0800

    NewGame button

--- app.js
+++ app.js
@@ -1,23 +1,38 @@
-//  Тоглогчийн ээлжийг хадгалах хувьсагч, нэгдүгээр тоглогч 0 , хоёрдугаар тоглогчийг 1 гэж тэмдэглэе
-var activePlayer = 0;
 
+var roundScore = 0;
 // Тоглогчдын цуглуулсан оноог хадгалах хувьсагч 
 var playerScores= [0 , 0];
 
 // Одоо эргүүлж буй тоглогчийн ээлжийн оноог хадгалах хувьсагч
-var roundScore = 0;
 
-// Шооны аль талаараа буусныг хадгалах хувьсагч, 1-6 утгыг энэ хувьсагчид санамсаргүйгээр үүсгэж өгнө
+var activePlayer = 0;
+var diceDom = document.querySelector(".dice");
 
+newGame();
+function newGame(){ 
+    roundScore = 0;
+    playerScores= [0 , 0];
+    activePlayer = 0;
 //window.document.querySelector('#score-0').textContent = dice;
 //Програм эхлэхэд бэлтгье  
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '0';
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
+    document.getElementById('name-0').textContent='Player 1';
+    document.getElementById('name-1').textContent='Player 2';
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+    document.getElementById('score-0').textContent = '0';
+    document.getElementById('score-1').textContent = '0';
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
 
-var diceDom = document.querySelector(".dice");
-diceDom.style.display = 'none';
+    document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
+    
+    document.querySelector('.player-0-panel').classList.add('active');
+    diceDom.style.display = "none";
+
+    
+}
+
 // Шоог шидэх эвент листенэр 
 
 function playerSwitchToNextPlayer(){
@@ -29,8 +44,10 @@ function playerSwitchToNextPlayer(){
     diceDom.style.display = "none";
 
 }
+
 document.querySelector('.btn-roll').addEventListener("click" ,function() 
 {   
+    // Шооны аль талаараа буусныг хадгалах хувьсагч, 1-6 утгыг энэ хувьсагчид санамсаргүйгээр үүсгэж өгнө
     var diceNumber = Math.floor(Math.random()*6) + 1;
     
     diceDom.style.display = "block";
@@ -48,12 +65,13 @@ document.querySelector('.btn-roll').addEventListener("click" ,function()
     }
 });
 
+
 //Hold товчны эвент листенэр
 
 document.querySelector('.btn-hold').addEventListener('click',function(){
 playerScores[activePlayer] = playerScores[activePlayer] + roundScore;
 document.getElementById('score-' +activePlayer).textContent = playerScores[activePlayer];  
-if(playerScores[activePlayer] >= 2){
+if(playerScores[activePlayer] >= 100){
     document.getElementById('name-' + activePlayer).textContent = "Winner";
     document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
     document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
@@ -68,7 +86,5 @@ else{
 
 //Шинэ тоглоом эхлүүлэх товчны эвент листенэр
 
-document.querySelector('.btn-new').addEventListener('click', function(){
-    
+document.querySelector('.btn-new').addEventListener('click', newGame);
 
-});
